4f255b5914b80e71f61af8c6060aed36fd5e451d
oswandrade <87822815+oswandrade@users.noreply.github.com>
2021-12-21 11:16:58 -0500

    Add files via upload

---
+++ Nomina/inicioNomina.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Nomina/mpNomina.master" AutoEventWireup="true" CodeFile="inicioNomina.aspx.cs" Inherits="Nomina_inicioNomina" %>
+
+<%@ Register Assembly="AjaxControlToolkit" Namespace="AjaxControlToolkit" TagPrefix="act1" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="contenidoPrincipal" Runat="Server">
+     <h1>Nómina</h1>
+</asp:Content>
+
---
+++ Nomina/mpNomina.master.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Nomina_mpNomina : System.Web.UI.MasterPage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            calificarOpciones();
+        }
+    }
+
+    #region CALIFICAR OPCIONES
+    protected void calificarOpciones()
+    {
+        /*CALIFICAR OPCIONES*/
+        string accion, grupo, menu, submenu, boton;
+
+        submenu = string.Empty;
+        boton = string.Empty;
+
+
+
+        accion = string.Empty;
+        grupo = (string)Session["Sgrupo"];
+
+        menu = "NOMINA";
+
+        /*submenu = "RETENCION";
+        lRetencion.Visible = LoginService.calificarOpcion(accion, grupo, menu, submenu, boton);*/
+
+        
+
+
+    }
+    #endregion  
+}
